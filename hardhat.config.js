e87a774a8dd2c37eced4e2dc5d248a0dceaad807
Richard Osahon Esele <richardriches12@gmail.com>
2025-07-20 19:28:59 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,13 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
 
